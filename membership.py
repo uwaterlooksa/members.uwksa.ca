d26d062536f3ae3cb0bb820916f970cd6c9da974
Bryan Kim <hi@bryankim.dev>
2024-08-14 17:17:04 -0400

    Fix login_required wrapper to use session instead of g

--- membership.py
+++ membership.py
@@ -28,7 +28,7 @@ oauth.register(
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if getattr(g, 'user', None) is None:
+        if session.get('user') is None:
             return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
